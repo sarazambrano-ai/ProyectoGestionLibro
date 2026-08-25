93e179aed00fb4881453421ab9fd87cbb4afb165
Misael Gallo <misaelgallo19@hotmail.com>
2026-08-24 19:00:40 -0500

    deshabilita edicion de celdas en tablas de listar libro y audiolibro

--- src/main/java/com/sxzo/gestionlibros/gui/GUIListarLibroAudio.java
+++ src/main/java/com/sxzo/gestionlibros/gui/GUIListarLibroAudio.java
@@ -59,9 +59,16 @@ public class GUIListarLibroAudio extends javax.swing.JFrame {
             Class[] types = new Class [] {
                 java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Double.class, java.lang.Integer.class, java.lang.String.class
             };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
             }
         });
         jScrollPane1.setViewportView(tblAudioLibro);
