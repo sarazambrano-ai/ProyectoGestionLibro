732472804524dca7c9181d3be32a203a6f498046
Carina Ortiz <carinaortiz@MacBook-Air.local>
2026-08-27 10:53:40 -0500

    Modificaciones en total pagar libro audio

--- src/main/java/com/sxzo/gestionlibros/gui/GUITotalPagarLibroAudio.java
+++ src/main/java/com/sxzo/gestionlibros/gui/GUITotalPagarLibroAudio.java
@@ -37,7 +37,7 @@ public class GUITotalPagarLibroAudio extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         txtIsbn = new javax.swing.JTextField();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("sansserif", 1, 14)); // NOI18N
         jLabel1.setText("Total a pagar libro fisico");
